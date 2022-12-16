2a47efeaf61c589b70475b05ead0a4bf036018bb
Cybill Clerger <he-is-here@hotmail.fr>
2022-12-16 18:08:35 +0100

    random grid

--- main.rb
+++ main.rb
@@ -3,6 +3,18 @@
 require './grid'
 require './cell'
 
+def init_random_cell
+  random_number = rand(0..2)
+  case random_number
+  when 0
+    nil
+  when 1
+    Cell.new(:dead)
+  else
+    Cell.new(:alive)
+  end
+end
+
 def run_until_death(grid)
   print_next_generation(grid) until grid.final_state? || grid.max_generation_reached?
   puts 'All good things must come to an end.' if grid.max_generation_reached?
@@ -33,38 +45,53 @@ def print_next_generation(grid)
   grid.print
 end
 
-# For now we only work with the following fixed grid (0 = no cell, 1 = dead cell, 2 = live cell)
+# Default grid (0 = no cell, 1 = dead cell, 2 = live cell)
 # 1	1	2	2	0
 # 0	2	1	1	1
 # 1	2	2	1	0
 # 1	1	2	0	0
 # 2	1	2	1	2
-# The expected next generation for this grid is
+# Expected next generation for this grid
 # 1 1 2 1 0
 # 0 2 1 2 1
 # 1 2 2 1 0
 # 1 1 2 0 0
 # 1 2 1 2 1
-size = 5
-cells = [
-  [Cell.new(:dead), nil, Cell.new(:dead), Cell.new(:dead), Cell.new(:alive)],
-  [Cell.new(:dead), Cell.new(:alive), Cell.new(:alive), Cell.new(:dead), Cell.new(:dead)],
-  [Cell.new(:alive), Cell.new(:dead), Cell.new(:alive), Cell.new(:alive), Cell.new(:alive)],
-  [Cell.new(:alive), Cell.new(:dead), Cell.new(:dead), nil, Cell.new(:dead)],
-  [nil, Cell.new(:dead), nil, nil, Cell.new(:alive)]
-]
-max_generation = 50
+
+init_mode = nil
+until %w[d r].include?(init_mode)
+  puts 'Do you want the default grid (d) or a random grid (r)?'
+  init_mode = gets.chomp.downcase
+  puts ''
+  case init_mode
+  when 'd'
+    size = 5
+    cells = [
+      [Cell.new(:dead), nil, Cell.new(:dead), Cell.new(:dead), Cell.new(:alive)],
+      [Cell.new(:dead), Cell.new(:alive), Cell.new(:alive), Cell.new(:dead), Cell.new(:dead)],
+      [Cell.new(:alive), Cell.new(:dead), Cell.new(:alive), Cell.new(:alive), Cell.new(:alive)],
+      [Cell.new(:alive), Cell.new(:dead), Cell.new(:dead), nil, Cell.new(:dead)],
+      [nil, Cell.new(:dead), nil, nil, Cell.new(:alive)]
+    ]
+    max_generation = 50
+  when 'r'
+    size = 5
+    cells = Array.new(size) { Array.new(size) { init_random_cell } }
+    max_generation = 50
+  else puts "Invalid answer: please type 'd' or 'r'"
+  end
+end
 
 grid = Grid.new(size, cells, max_generation)
 puts 'Welcome! Here is your starting grid:'
 grid.print
 
-mode = nil
-until %w[d s].include?(mode)
+run_mode = nil
+until %w[d s].include?(run_mode)
   puts 'Do you want the simulation to run until all the cells die (d) or do you want to run it step-by-step (s)?'
-  mode = gets.chomp.downcase
+  run_mode = gets.chomp.downcase
   puts ''
-  case mode
+  case run_mode
   when 'd' then run_until_death(grid)
   when 's' then run_step_by_step(grid)
   else puts "Invalid answer: please type 'd' or 's'"
